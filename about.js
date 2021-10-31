c36b0940e203bc5f78743ad6753ed345a519e8f1
Elijah Jackson <elijahdj88@gmail.com>
2021-10-31 11:49:39 -0400

    completed part 2

--- about.js
+++ about.js
@@ -5,10 +5,15 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('form submitted successfully!');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function mouseOver() {
+	document.getElementById("catPic")
+	alert("you doing good")
+  }
